43340e53eaf8f800a0c72b93f3b9dfc90f048a88
Claude Stephane Kouame <claudestephane2010@live.fr>
2023-12-02 20:07:50 +0100

    Add multiple dirs test feature. And update arg style

--- tester_template.sh
+++ tester_template.sh
@@ -12,15 +12,16 @@ Usage: ./run_tests.sh  OPTION...
 Performs all tests according to the option passed with the reference machine and the user's machine, compares the 2 results and attests whether the user's compiler behaves as the reference does
 
 Options:
-  --tokens            Phase 1: Scans for tokens and prints them.
-  --parse             Phase 2: Parses the stream of tokens to check for syntax errors.
-  --absyn             Phase 3: Creates an abstract syntax tree from the input tokens and prints it.
-  --tables            Phase 4a: Builds a symbol table and prints its entries.
-  --semant            Phase 4b: Performs the semantic analysis.
-  --vars              Phase 5: Allocates memory space for variables and prints the amount of allocated memory.
-  build		      Build the user compiler
-  --help              Show this help.
-  --version           Show tester version
+  tokens            Phase 1: Scans for tokens and prints them.
+  parse             Phase 2: Parses the stream of tokens to check for syntax errors.
+  absyn             Phase 3: Creates an abstract syntax tree from the input tokens and prints it.
+  tables            Phase 4a: Builds a symbol table and prints its entries.
+  semant            Phase 4b: Performs the semantic analysis.
+  vars              Phase 5: Allocates memory space for variables and prints the amount of allocated memory.
+  codegen	    Phase 6: Generate assembly code
+  build		    Build the user compiler
+  --help            Show this help.
+  --version         Show tester version
 EOF
     exit 0
 }
@@ -30,7 +31,7 @@ version() {
 }
 
 # NOT TO BE RUN
-echo "This script is no to be run. Please use 'run_tests.sh instead!'" && exit 0
+echo "This script is no to be run. Please use 'run_tests.sh' instead!" && exit 0
 
 build=false
 
@@ -38,23 +39,23 @@ for ((i = 1; i <= "$#"; i++)); do
     arg="${!i}"
 
     if [[ $arg = --* ]]; then
-        case "$arg" in
-	    "--tokens") ;&
-	    "--parse") ;&
-            "--absyn") ;&
-            "--tables") ;&
-            "--semant") ;&
-            "--vars")
-		phase=$arg
-            ;;
-        esac
 	case "$arg" in
 	    "--help") help ;;
 	    "--version") version ;;	
 	esac
 
-        options="$options $arg"
+        #options="$options $arg"
     else
+	case "$arg" in
+	    "tokens") phase="--tokens" ;;
+	    "parse") phase="--parse" ;;
+            "absyn") phase="--absyn" ;;
+            "tables") phase="--tables" ;;
+            "semant") phase="--semant" ;;
+            "vars") phase="--vars" ;;
+	    "codegen") phase="--codegen" ;;
+        esac
+
         [ $arg == "build" ] && build=true 
     fi
 done
@@ -64,8 +65,8 @@ done
 runtime_test_dir="$curr_dir/spl-testfiles/runtime_tests"
 semant_test_dir="$curr_dir/spl-testfiles/semant_errors"
 syntax_error_test_dir="$curr_dir/spl-testfiles/syntax_errors"
-
 codegen_test_dir="$curr_dir/spl-testfiles/runtime_tests"
+
 assembler="$curr_dir/eco32tools/compile_s.sh"
 
 java_ref_path="$curr_dir/java_ref"
@@ -76,26 +77,18 @@ user_java_compiler_path="__user_java_compiler_path__"
 
 compiler_written_lang="__comp_lang__"
 
-tests_1=($(ls $runtime_test_dir | grep .*.spl))
-tests_2=($(ls $semant_test_dir | grep .*.spl))
-tests_3=($(ls $syntax_error_test_dir | grep .*.spl))
-tests_4=($(ls $codegen_test_dir | grep .*.bin))
 
-if [ "$phase" == "--tables" ]; then
-  tests_dir="$runtime_test_dir"
-  tests_dir_without_path="${tests_1[*]}"
-elif [ "$phase" == "--semant" ]; then
-  tests_dir="$semant_test_dir"
-  tests_dir_without_path="${tests_2[*]}"
-elif [ "$phase" == "--vars" ]; then
-  tests_dir="$runtime_test_dir"
-  tests_dir_without_path="${tests_1[*]}"
+if [ "$phase" == "--semant" ]; then
+    tests_dirs="$runtime_test_dir $semant_test_dir"
+elif [ "$phase" == "--parse" ]; then
+    tests_dirs="$runtime_test_dir $syntax_error_test_dir"
 else
-  tests_dir="$runtime_test_dir"
-  tests_dir_without_path="${tests_1[*]}"
+  tests_dirs="$runtime_test_dir"
 fi
 
-echo "tests_dir: $tests_dir phase: $phase" | tee test_results.log
+tests_dirs_arr=($(echo $tests_dirs | tr ' ' '\n'))
+
+echo "tests_dirs: $tests_dirs phase: $phase" | tee test_results.log
 
 : '
 execute cmd and return res and exit code 
@@ -169,46 +162,58 @@ run_c_ref_compiler() {
 
 
 echo "YOUR TURN" | tee -a test_results.log
-for f in ${tests_dir_without_path[*]}; do
-  echo "$tests_dir/$f"
-  if [ "$phase" == "--codegen" ] && [ "$f" == "lambda.spl" ]; then
-      echo "0" >"${tmpdir}/${f}_own"
-  else
-      if [ "$compiler_written_lang" == "java" ]; then
-	  tmp_own_res=$(run_user_java_compiler "$f" "$tests_dir")
-      else
-	  tmp_own_res=$(run_user_c_compiler "$f" "$tests_dir")
-      fi
-      echo "$tmp_own_res" >"${tmpdir}/${f}_own"
-  fi
+for test_dir in ${tests_dirs_arr[*]}; do
+    tests=($(ls $test_dir | grep .*.spl))
+    test_dir_without_path="${tests[*]}"
+    for f in ${test_dir_without_path[*]}; do
+	echo "$test_dir/$f"
+	if [ "$phase" == "--codegen" ] && [ "$f" == "lambda.spl" ]; then
+	    echo "0" >"${tmpdir}/${f}_own"
+	else
+	    if [ "$compiler_written_lang" == "java" ]; then
+		tmp_own_res=$(run_user_java_compiler "$f" "$test_dir")
+	    else
+		tmp_own_res=$(run_user_c_compiler "$f" "$test_dir")
+	    fi
+	    echo "$tmp_own_res" >"${tmpdir}/${f}_own"
+	fi
 
+    done
 done
 
 
 echo "REF TURN" | tee -a test_results.log
-for f in ${tests_dir_without_path[*]}; do
-  echo "$tests_dir/$f"
-  if [ "$phase" == "--codegen" ] && [ "$f" == "lambda.spl" ]; then
-      echo "0" >"${tmpdir}/${f}_ref"
-  else
-    tmp_ref_res=$(run_c_ref_compiler "$f" "$tests_dir")
-    echo "$tmp_ref_res" >"${tmpdir}/${f}_ref"
-  fi
+for test_dir in ${tests_dirs_arr[*]}; do
+    tests=($(ls $test_dir | grep .*.spl))
+    test_dir_without_path="${tests[*]}"
+    for f in ${test_dir_without_path[*]}; do
+	echo "$test_dir/$f"
+	if [ "$phase" == "--codegen" ] && [ "$f" == "lambda.spl" ]; then
+	    echo "0" >"${tmpdir}/${f}_ref"
+	else
+	    tmp_ref_res=$(run_c_ref_compiler "$f" "$test_dir")
+	    echo "$tmp_ref_res" >"${tmpdir}/${f}_ref"
+	fi
 
+    done
 done
 
 
 passed=0
 nbr_test=0
-for f in ${tests_dir_without_path[*]}; do
-  tmp_res=$(diff "${tmpdir}/${f}_own" "${tmpdir}/${f}_ref")
-  if [ -z "$tmp_res" ]; then
-    echo "TEST: $f $tmp_res is fine" | tee -a test_results.log
-    passed=`expr $passed + 1`
-  else
-    echo -e "TEST: $f is not fine \n $tmp_res " | tee -a test_results.log
-  fi
-  nbr_test=`expr $nbr_test + 1`
+for test_dir in ${tests_dirs_arr[*]}; do
+    tests=($(ls $test_dir | grep .*.spl))
+    test_dir_without_path="${tests[*]}"
+    for f in ${test_dir_without_path[*]}; do
+	tmp_res=$(diff "${tmpdir}/${f}_own" "${tmpdir}/${f}_ref")
+	if [ -z "$tmp_res" ]; then
+	    echo "TEST: $f $tmp_res is fine" | tee -a test_results.log
+	    passed=`expr $passed + 1`
+	else
+	    echo -e "TEST: $f is not fine \n $tmp_res " | tee -a test_results.log
+	fi
+	nbr_test=`expr $nbr_test + 1`
+    done
 done
 
 #echo -e "\n TMPDIR $tmpdir"
@@ -218,5 +223,4 @@ echo "removing directory $tmpdir ..." && rm -rf "${tmpdir}"
 
 echo -e "\nFINAL RES FOR $phase"
 echo "PASSED ( $passed/$nbr_test )"
-
 
